7b4fb6e874d8d0abeef20d568c5b87234ebee4f5
Daniil Shevchenko <daniil.shevchenko.shi.2023@lpnu.ua>
2026-03-31 15:35:51 +0300

    fix: fixed demonstration of trains with no free places (QA bugfix)

--- SearchService.cs
+++ SearchService.cs
@@ -4,5 +4,6 @@ namespace UaRailways.Core
         //Logic to filter trains by stations
         //Ensure From and To stations are not the same
         //QA Fix: Ensure UTC Time Format
+        //QA Fix: Fixed logic for demonstration of trains with no free places
     }
 }
